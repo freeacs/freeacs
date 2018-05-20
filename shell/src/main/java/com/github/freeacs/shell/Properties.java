e6bf327627d77f96b19314d335bb4d65b052e8cd
Jarl André Hübenthal <jarl.andre@gmail.com>
2018-05-20 20:55:42 +0200

    fix shell module (#84)
    * fix shell module
    
    * whitespace
    
    * headless
    
    * fix

--- shell/src/main/java/com/github/freeacs/shell/Properties.java
+++ shell/src/main/java/com/github/freeacs/shell/Properties.java
@@ -7,7 +7,7 @@ public class Properties {
 
 	private static String propertyfile = "xaps-shell.properties";
 
-	private static PropertyReader pr;
+	public static PropertyReader pr;
 	static {
 		try {
 			pr = new PropertyReader(propertyfile);
@@ -17,17 +17,6 @@ public class Properties {
 	}
 
 	private static Logger logger = LoggerFactory.getLogger(Properties.class);
-
-	@SuppressWarnings("unused")
-	private static int getInteger(String propertyKey, int defaultValue) {
-		try {
-			String prop = pr.getProperty(propertyKey);
-			return Integer.parseInt(prop);
-		} catch (Throwable t) {
-			logger.warn("The value of " + propertyKey + " was not a number, instead using default value " + defaultValue);
-			return defaultValue;
-		}
-	}
 
 	private static String getString(String propertyKey, String defaultValue) {
 		try {
@@ -44,28 +33,6 @@ public class Properties {
 
 	public static boolean isRestricted() {
 		return getString("restricted", "false").equals("true");
-	}
-	
-	public static String getSyslogServer() {
-		String ss = getString("syslog.server", "localhost:9116");
-		int colonPos = ss.indexOf(":");
-		if (colonPos == -1)
-			return "localhost";
-		else
-			return ss.substring(0, colonPos);
-	}
-
-	public static int getSyslogServerPort() {
-		String ss = getString("syslog.server", "localhost:9116");
-		int colonPos = ss.indexOf(":");
-		if (colonPos == -1)
-			return 9116;
-		else
-			try {
-				return Integer.parseInt(ss.substring(colonPos + 1));
-			} catch (NumberFormatException nfe) {
-				return 9116;
-			}
 	}
 
 
