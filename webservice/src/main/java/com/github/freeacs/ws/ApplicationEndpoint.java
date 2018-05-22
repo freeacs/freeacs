843b173f532f83c01701b97718f4e86b23d2f3dd
Jarl André Hübenthal <jarl.andre@gmail.com>
2018-05-22 23:23:49 +0200

    implementing some response data

--- webservice/src/main/java/com/github/freeacs/ws/ApplicationEndpoint.java
+++ webservice/src/main/java/com/github/freeacs/ws/ApplicationEndpoint.java
@@ -1,21 +1,36 @@
 package com.github.freeacs.ws;
 
 
-import com.github.freeacs.ws.xml.GetUnittypesRequest;
-import com.github.freeacs.ws.xml.GetUnittypesResponse;
+import com.github.freeacs.ws.xml.*;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import javax.validation.Valid;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
 
 
 @Endpoint
 public class ApplicationEndpoint {
     private static final String NAMESPACE_URI = "http://xml.ws.freeacs.github.com/";
 
-    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getUnittypesRequest")
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "GetUnittypesRequest")
     @ResponsePayload
     public GetUnittypesResponse getCountry(@RequestPayload GetUnittypesRequest request) {
-        return new GetUnittypesResponse();
+        ObjectFactory factory = new ObjectFactory();
+        UnittypeList unittypeList = factory.createUnittypeList();
+        ArrayOfUnittype arrayOfUnittype = factory.createArrayOfUnittype();
+        Unittype unittype = factory.createUnittype();
+        unittype.setDescription(factory.createUnittypeDescription("Test"));
+        unittype.setProtocol(factory.createUnittypeProtocol("Protocol"));
+        unittype.setName("tEST");
+        arrayOfUnittype.getItem().add(unittype);
+        unittypeList.setUnittypeArray(arrayOfUnittype);
+        JAXBElement<UnittypeList> unittypeListElem = factory.createGetUnittypesResponseUnittypes(unittypeList);
+        GetUnittypesResponse response = new GetUnittypesResponse();
+        response.setUnittypes(unittypeListElem);
+        return response;
     }
 }
