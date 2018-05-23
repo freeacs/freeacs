0431cb63ff641e6e92697370b490a6ca0c1408a8
Jarl André Hübenthal <jarl.andre@gmail.com>
2018-05-23 13:03:32 +0200

    adding test

--- webservice/src/main/java/com/github/freeacs/ws/Application.java
+++ webservice/src/main/java/com/github/freeacs/ws/Application.java
@@ -2,8 +2,9 @@ package com.github.freeacs.ws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = FlywayAutoConfiguration.class)
 public class Application {
 
     public static void main(String[] args) {
---
+++ webservice/src/test/java/com/github/freeacs/ws/FlywayConfiguration.java
@@ -0,0 +1,31 @@
+package com.github.freeacs.ws;
+
+import org.flywaydb.core.Flyway;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
+
+@Configuration
+public class FlywayConfiguration {
+
+    private final DataSource syslogDataSource;
+    private final DataSource mainDataSource;
+
+    @Autowired
+    public FlywayConfiguration(@Qualifier("main") DataSource mainDataSource, @Qualifier("syslog") DataSource syslogDataSource) {
+        this.mainDataSource = mainDataSource;
+        this.syslogDataSource = syslogDataSource;
+    }
+
+    @PostConstruct
+    public void runFlyway() {
+        Flyway flyway = new Flyway();
+        flyway.setDataSource(mainDataSource);
+        flyway.migrate();
+        flyway.setDataSource(syslogDataSource);
+        flyway.migrate();
+    }
+}
