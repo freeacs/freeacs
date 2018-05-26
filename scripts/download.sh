53ad8d059cf723412bc5957c79bad95827eb627b
Jarl André Hübenthal <jarl.andre@gmail.com>
2018-05-26 23:35:05 +0200

    improving script

--- scripts/download.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-whattodownload="-bin|tables|shell"
-curl -s https://api.github.com/repos/freeacs/freeacs/releases/latest | jq -r ".assets[] | select(.name | test(\"${whattodownload}\")) | .browser_download_url" > files.txt
-awk '{print $0;}' files.txt | xargs -l1 wget
-unzip "*.zip"
-rm -rf *.zip
