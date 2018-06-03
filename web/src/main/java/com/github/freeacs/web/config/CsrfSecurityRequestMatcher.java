257fd1651e727e27f53bf8d60bc4fdc731525249
jarlah <jarlah@debian>
2018-06-03 08:54:31 +0200

    making web work nicely with nginx

--- web/src/main/java/com/github/freeacs/web/config/CsrfSecurityRequestMatcher.java
+++ web/src/main/java/com/github/freeacs/web/config/CsrfSecurityRequestMatcher.java
@@ -1,5 +1,7 @@
 package com.github.freeacs.web.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.web.util.matcher.RegexRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
@@ -7,14 +9,19 @@ import javax.servlet.http.HttpServletRequest;
 import java.util.regex.Pattern;
 
 public class CsrfSecurityRequestMatcher implements RequestMatcher {
+    private static Logger logger = LoggerFactory.getLogger(CsrfSecurityRequestMatcher.class);
+
     private Pattern allowedMethods = Pattern.compile("^(GET|HEAD|TRACE|OPTIONS)$");
     private RegexRequestMatcher unprotectedMatcher = new RegexRequestMatcher("^(?!/(login|logout)*$).*", null);
 
     @Override
     public boolean matches(HttpServletRequest request) {
+        logger.info(request.getRequestURI());
         if(allowedMethods.matcher(request.getMethod()).matches()){
             return false;
         }
-        return !unprotectedMatcher.matches(request);
+        boolean matches = !unprotectedMatcher.matches(request);
+        logger.info("Matches: " + matches);
+        return matches;
     }
 }
