ec403ac486082be18dff1ed9613bb2e547c4a33f
Jarl André Hübenthal <noreply@github.com>
2018-08-01 09:54:59 +0200

    adding monitor in installation scripts

--- scripts/install_centos.sh
+++ scripts/install_centos.sh
@@ -179,6 +179,9 @@ echo "
     location /web/ {
       proxy_pass http://localhost:8081/web/;
     }
+    location /monitor/ {
+      proxy_pass http://localhost:8090/monitor/;
+    }
     location /webservice/ {
       proxy_pass http://localhost:8088/webservice/;
     }
@@ -214,6 +217,9 @@ http {
     }
     location /web/ {
       proxy_pass http://localhost:8081/web/;
+    }
+    location /monitor/ {
+      proxy_pass http://localhost:8090/monitor/;
     }
     location /webservice/ {
       proxy_pass http://localhost:8088/webservice/;
