6a46831f60d1d227abbc390daed141bf504a763b
Jarl André Hübenthal <jarl.andre@gmail.com>
2018-10-05 17:05:28 +0200

    formatting

--- tr069/src/test/java/com/github/freeacs/base/db/DBAccessSessionTR069Test.java
+++ tr069/src/test/java/com/github/freeacs/base/db/DBAccessSessionTR069Test.java
@@ -1,20 +1,20 @@
 package com.github.freeacs.base.db;
 
-import org.junit.Test;
-
 import static org.junit.Assert.*;
+
+import org.junit.Test;
 
 public class DBAccessSessionTR069Test {
 
-    @Test
-    public void parseUnittypeName() {
-        // Given:
-        String unittypeNameStr = "ODU/xxx/yyy";
+  @Test
+  public void parseUnittypeName() {
+    // Given:
+    String unittypeNameStr = "ODU/xxx/yyy";
 
-        // When:
-        String parsedUnittypeName = DBAccessSessionTR069.parseUnittypeName(unittypeNameStr);
+    // When:
+    String parsedUnittypeName = DBAccessSessionTR069.parseUnittypeName(unittypeNameStr);
 
-        // Then:
-        assertEquals("ODU-xxx-yyy", parsedUnittypeName);
-    }
+    // Then:
+    assertEquals("ODU-xxx-yyy", parsedUnittypeName);
+  }
 }
