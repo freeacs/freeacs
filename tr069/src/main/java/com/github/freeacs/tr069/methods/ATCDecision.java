74ad9b24affa92175982c80c1bd6c20e821e5d3f
Jarl André Hübenthal <noreply@github.com>
2018-10-12 20:02:22 +0200

    adding AutonomousTransferComplete

--- tr069/src/main/java/com/github/freeacs/tr069/methods/ATCDecision.java
+++ tr069/src/main/java/com/github/freeacs/tr069/methods/ATCDecision.java
@@ -4,15 +4,15 @@ import com.github.freeacs.base.Log;
 import com.github.freeacs.tr069.HTTPReqResData;
 import com.github.freeacs.tr069.xml.Fault;
 
-public class TCDecision {
+public class ATCDecision {
   public static void process(HTTPReqResData reqRes) {
 
     try {
       Fault fault = reqRes.getRequest().getFault();
       if (fault != null && !fault.getFaultCode().equals("0")) {
-        String errormsg = "TC request reports a faultcode (" + fault.getFaultCode();
+        String errormsg = "ATC request reports a faultcode (" + fault.getFaultCode();
         errormsg += ") with faultstring (" + fault.getFaultString() + ")";
-        Log.error(TCDecision.class, errormsg);
+        Log.error(ATCDecision.class, errormsg);
       }
     } finally {
       reqRes.getResponse().setMethod(TR069Method.AUTONOMOUS_TRANSFER_COMPLETE);
