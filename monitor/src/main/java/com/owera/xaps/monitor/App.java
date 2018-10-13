0fb2c0c67a6140aa1427681aa92499862f284b29
Jarl André Hübenthal <jarlah@users.noreply.github.com>
2018-10-13 20:48:41 +0200

    remove embedded jetty stuff on monitor and stun

--- monitor/src/main/java/com/owera/xaps/monitor/App.java
+++ monitor/src/main/java/com/owera/xaps/monitor/App.java
@@ -4,20 +4,17 @@ import static com.github.freeacs.common.spark.ResponseHelper.process;
 import static spark.Spark.path;
 
 import com.github.freeacs.common.http.SimpleResponseWrapper;
-import com.github.freeacs.common.jetty.JettyFactory;
 import com.github.freeacs.common.util.Sleep;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import javax.servlet.ServletException;
 import spark.Spark;
-import spark.embeddedserver.EmbeddedServers;
 
 public class App {
 
   public static void main(String[] args) throws ServletException {
     Config config = ConfigFactory.load();
     Spark.port(config.getInt("server.port"));
-    EmbeddedServers.add(EmbeddedServers.Identifiers.JETTY, new JettyFactory(true, -1, -1));
     routes(new Properties(config));
     Runtime.getRuntime()
         .addShutdownHook(
@@ -26,7 +23,6 @@ public class App {
                   System.out.println("Shutdown Hook is running !");
                   Sleep.terminateApplication();
                 }));
-    System.out.println("Application Terminating ...");
   }
 
   public static void routes(Properties properties) throws ServletException {
--- stun/src/main/java/com/github/freeacs/stun/App.java
+++ stun/src/main/java/com/github/freeacs/stun/App.java
@@ -3,19 +3,16 @@ package com.github.freeacs.stun;
 import static spark.Spark.get;
 
 import com.github.freeacs.common.hikari.HikariDataSourceHelper;
-import com.github.freeacs.common.jetty.JettyFactory;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import javax.sql.DataSource;
 import spark.Spark;
-import spark.embeddedserver.EmbeddedServers;
 
 public class App {
 
   public static void main(String[] args) {
     Config config = ConfigFactory.load();
     Spark.port(config.getInt("server.port"));
-    EmbeddedServers.add(EmbeddedServers.Identifiers.JETTY, new JettyFactory(true, -1, -1));
     DataSource mainDs = HikariDataSourceHelper.dataSource(config.getConfig("main"));
     Properties properties = new Properties(config);
     StunServlet stunServlet = new StunServlet(mainDs, properties);
