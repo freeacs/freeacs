968a654b70d55da06a52adce9f27a2e228728194
Jarl André Hübenthal <noreply@github.com>
2018-10-16 21:39:01 +0200

    add KickUnitRequest to webservice (#205)
    * add KickUnitRequest to webservice
    
    * format
    
    * adding test for kick
    
    * require unit to be found
    
    * format
    
    * rename
    
    * remove unused exception
    
    * remove unused exception

--- webservice/src/test/java/com/github/freeacs/ws/WebserviceIntegrationTest.java
+++ webservice/src/test/java/com/github/freeacs/ws/WebserviceIntegrationTest.java
@@ -7,6 +7,10 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -21,5 +25,30 @@ public class WebserviceIntegrationTest {
   public void testWsdlPage() {
     String body = this.restTemplate.getForObject("/ws/acs.wsdl", String.class);
     assertThat(body).contains("GetUnittypesRequest");
+  }
+
+  @Test
+  public void testKick() {
+    String xml =
+        "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" "
+            + "xmlns:x.=\"http://xml.ws.freeacs.github.com/\">\n"
+            + "   <soapenv:Header/>\n"
+            + "   <soapenv:Body>\n"
+            + "      <x.:KickUnitRequest>\n"
+            + "         <x.:login>\n"
+            + "            <x.:username>admin</x.:username>\n"
+            + "            <!--Optional:-->\n"
+            + "            <x.:password>freeacs</x.:password>\n"
+            + "         </x.:login>\n"
+            + "         <x.:unitId>test</x.:unitId>\n"
+            + "      </x.:KickUnitRequest>\n"
+            + "   </soapenv:Body>\n"
+            + "</soapenv:Envelope>";
+    HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(MediaType.TEXT_XML);
+    HttpEntity<String> httpEntity = new HttpEntity<>(xml, headers);
+    ResponseEntity<String> body =
+        this.restTemplate.postForEntity("/ws/acs", httpEntity, String.class);
+    assertThat(body.getBody()).contains("KickUnitResponse");
   }
 }
