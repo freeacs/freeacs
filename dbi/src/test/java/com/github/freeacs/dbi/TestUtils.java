90a4f8c01f402077b99b89bce13dffe4cdda2397
Jarl André Hübenthal <jarl.andre@gmail.com>
2018-10-21 20:49:20 +0200

    adding unit job tests + refactor testutils

--- dbi/src/test/java/com/github/freeacs/dbi/TestUtils.java
+++ dbi/src/test/java/com/github/freeacs/dbi/TestUtils.java
@@ -1,6 +1,10 @@
 package com.github.freeacs.dbi;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 import java.sql.SQLException;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -42,5 +46,62 @@ public class TestUtils {
     Profiles profiles = unittype.getProfiles();
     profiles.addOrChangeProfile(profile, acs);
     return profile;
+  }
+
+  public static Unit createUnitAndVerify(ACSUnit acsUnit, String unitId, Profile profile)
+      throws SQLException {
+    // When:
+    acsUnit.addUnits(Collections.singletonList(unitId), profile);
+    // Then:
+    Unit unit = acsUnit.getUnitById(unitId);
+    assertEquals(unitId, unit.getId());
+    assertEquals(profile.getName(), unit.getProfile().getName());
+    assertEquals(profile.getUnittype().getName(), unit.getUnittype().getName());
+    return unit;
+  }
+
+  public static Unit createUnitAndVerify(
+      ACSUnit acsUnit, String unitId, ACS acs, String unitTypeName, String profileName)
+      throws SQLException {
+    Unittype unittype = createUnittype(unitTypeName, acs);
+    Profile profile = unittype.getProfiles().getByName(profileName);
+    return createUnitAndVerify(acsUnit, unitId, profile);
+  }
+
+  public static Job createJobAndVerify(
+      String jobName, JobFlag jobFlag, String jobDesc, Unittype unittype, Group group, ACS acs)
+      throws SQLException {
+    Jobs jobs = unittype.getJobs();
+    Job job = new Job(unittype, jobName, jobFlag, jobDesc, group, 60, "n1", null, null, 0, 0);
+    jobs.add(job, acs);
+    job = jobs.getByName(jobName);
+    assertNotNull(job);
+    return job;
+  }
+
+  public static Group createGroupAndVerify(
+      String groupName, String profileName, Unittype unittype, ACS acs) throws SQLException {
+    Groups groups = unittype.getGroups();
+    Profile profile = unittype.getProfiles().getByName(profileName);
+    Group group = new Group(groupName, groupName, null, unittype, profile);
+    groups.addOrChangeGroup(group, acs);
+    group = groups.getByName(groupName);
+    assertNotNull(group);
+    return group;
+  }
+
+  public static UnitJob createUnitJobAndVerify(UnitJobs unitJobs, String unitId, int jobId)
+      throws SQLException {
+    UnitJob unitJob = new UnitJob();
+    unitJob.setUnitId(unitId);
+    unitJob.setJobId(jobId);
+    unitJob.setUnconfirmedFailed(1);
+    unitJob.setConfirmedFailed(1);
+    unitJobs.addOrChange(unitJob);
+    Job job = new Job();
+    job.setId(jobId);
+    List<UnitJob> jobs = unitJobs.readAllUnprocessed(job);
+    assertEquals(1, jobs.size());
+    return unitJob;
   }
 }
