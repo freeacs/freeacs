fb4f8ecadfd94723c4d60935fa3daff8594dd2af
Jarl André Hübenthal <jarlah@users.noreply.github.com>
2018-10-21 19:42:13 +0200

    adding test skeletons for the most needed classes in dbi

---
+++ dbi/src/test/java/com/github/freeacs/dbi/UsersTest.java
@@ -0,0 +1,14 @@
+package com.github.freeacs.dbi;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UsersTest {
+
+  @Test
+  public void delete() {}
+
+  @Test
+  public void addOrChange() {}
+}
