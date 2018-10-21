fc369471ada3ee730209c2e9393ec4959ea94c6d
Jarl André Hübenthal <noreply@github.com>
2018-10-21 21:02:14 +0200

    adding unit job tests + refactor testutils  (#212)
    * adding unit job tests + refactor testutils
    
    * use variable
    
    * test utils should be abstract

--- dbi/src/test/java/com/github/freeacs/dbi/UnitJobsTest.java
+++ dbi/src/test/java/com/github/freeacs/dbi/UnitJobsTest.java
@@ -2,10 +2,36 @@ package com.github.freeacs.dbi;
 
 import static org.junit.Assert.*;
 
+import java.sql.SQLException;
+import java.util.List;
 import org.junit.Test;
 
-public class UnitJobsTest {
+public class UnitJobsTest extends BaseDBITest {
 
   @Test
-  public void addOrChange() {}
+  public void addOrChange() throws SQLException {
+    // Given:
+    String unitId = "YidduJb7sssC";
+    String unittypeName = "Test unittype";
+    String groupName = "Group";
+    String profileName = "Default";
+    String jobName = "Job";
+    JobFlag jobFlag = new JobFlag("CONFIG|REGULAR");
+    String jobDesc = "Description";
+
+    // When:
+    Unittype unittype = TestUtils.createUnittype(unittypeName, acs);
+    Profile profile = unittype.getProfiles().getByName(profileName);
+    ACSUnit acsUnit = new ACSUnit(dataSource, acs, syslog);
+    TestUtils.createUnitAndVerify(acsUnit, unitId, profile);
+    Group group = TestUtils.createGroupAndVerify(groupName, profileName, unittype, acs);
+    Job job = TestUtils.createJobAndVerify(jobName, jobFlag, jobDesc, unittype, group, acs);
+    UnitJobs unitJobs = new UnitJobs(dataSource);
+    TestUtils.createUnitJobAndVerify(unitJobs, unitId, job.getId());
+
+    // Then:
+    List<UnitJob> unitJobList = unitJobs.readAllUnprocessed(job);
+    assertEquals(1, unitJobList.size());
+    assertEquals(unitId, unitJobList.get(0).getUnitId());
+  }
 }
