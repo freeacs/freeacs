0e78355a4e3e61e270b8d1825ab13a9b49901fc1
Jarl André Hübenthal <jarlah@users.noreply.github.com>
2018-11-17 22:40:15 +0100

    remove dead code

--- web/src/main/java/com/github/freeacs/web/app/page/user/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.github.freeacs.web.app.page.user;
-
-/** The Class ResourceNotFoundException. */
-// @ResponseStatus(HttpStatus.NOT_FOUND)
-public class ResourceNotFoundException extends RuntimeException {
-  /** The Constant serialVersionUID. */
-  private static final long serialVersionUID = 1L;
-}
