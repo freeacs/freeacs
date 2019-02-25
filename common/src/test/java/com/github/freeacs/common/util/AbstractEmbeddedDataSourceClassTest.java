85f26ab039d01790989d3b2c6476c92dc50edadc
Jarl André Hübenthal <jarlah@users.noreply.github.com>
2019-02-25 18:51:03 +0100

    trying to move xmls out of test

--- common/src/test/java/com/github/freeacs/common/util/AbstractEmbeddedDataSourceClassTest.java
+++ common/src/test/java/com/github/freeacs/common/util/AbstractEmbeddedDataSourceClassTest.java
@@ -4,6 +4,12 @@ import ch.vorburger.exec.ManagedProcessException;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public abstract class AbstractEmbeddedDataSourceClassTest extends AbstractEmbeddedDataSourceHelper {
 
     @BeforeClass
@@ -14,5 +20,12 @@ public abstract class AbstractEmbeddedDataSourceClassTest extends AbstractEmbedd
     @AfterClass
     public static void tearDownAfterClass() throws Exception {
         AbstractEmbeddedDataSourceHelper.tearDownAfterClass();
+    }
+
+    public String getFileAsString(final String name) throws IOException {
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource(name).getFile());
+        byte[] encoded = Files.readAllBytes(Path.of(file.toURI()));
+        return new String(encoded, StandardCharsets.UTF_8);
     }
 }
---
+++ tr069/src/test/java/com/github/freeacs/AutonmousTransferCompleteTest.java
@@ -0,0 +1,6 @@
+package com.github.freeacs;
+
+import com.github.freeacs.common.util.AbstractEmbeddedDataSourceClassTest;
+
+public class AutonmousTransferCompleteTest extends AbstractEmbeddedDataSourceClassTest {
+}
