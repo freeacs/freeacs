d8fb82e95e668ea6093be0277d16a025761eb185
Jarl André Hübenthal <jarlah@users.noreply.github.com>
2019-02-25 21:19:54 +0100

    removing placeholder

--- tr069/src/test/java/com/github/freeacs/AutonmousTransferCompleteTest.java
+++
@@ -1,6 +0,0 @@
-package com.github.freeacs;
-
-import com.github.freeacs.common.util.AbstractEmbeddedDataSourceClassTest;
-
-public class AutonmousTransferCompleteTest extends AbstractEmbeddedDataSourceClassTest {
-}
