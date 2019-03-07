a918910a417db33831fc6aca991a41090a5b8363
Jarl André Hübenthal <jarlah@users.noreply.github.com>
2019-03-07 07:07:06 +0100

    adding spring boot maven plugin

--- syslog/src/main/java/com/github/freeacs/syslogserver/App.java
+++ syslog/src/main/java/com/github/freeacs/syslogserver/App.java
@@ -5,19 +5,19 @@ import static spark.Spark.get;
 import com.github.freeacs.common.hikari.HikariDataSourceHelper;
 import com.github.freeacs.common.scheduler.ExecutorWrapper;
 import com.github.freeacs.common.scheduler.ExecutorWrapperFactory;
+import com.github.freeacs.common.spark.SparkApp;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import javax.sql.DataSource;
 import spark.Spark;
 
-public class App {
+public class App extends SparkApp {
+
   public static void main(String[] args) {
-    Config config = ConfigFactory.load();
-    Spark.port(config.getInt("server.port"));
-    DataSource mainDs = HikariDataSourceHelper.dataSource(config.getConfig("main"));
-    Properties properties = new Properties(config);
+    final App app = new App();
+    Properties properties = new Properties(app.config);
     ExecutorWrapper executorWrapper = ExecutorWrapperFactory.create(3);
-    SyslogServlet syslogServlet = new SyslogServlet(mainDs, properties, executorWrapper);
+    SyslogServlet syslogServlet = new SyslogServlet(app.datasource, properties, executorWrapper);
     syslogServlet.init();
     get(properties.getContextPath() + "/ok", (req, res) -> syslogServlet.health());
     Runtime.getRuntime()
