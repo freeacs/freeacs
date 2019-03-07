7544f48ea238e36aa108c6c1c423742cdf4d0b4e
Jarl André Hübenthal <jarlah@users.noreply.github.com>
2019-03-07 20:08:01 +0100

    implementing assembly for the other modules

---
+++ core/src/main/scripts/start.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+java -jar \
+-Xms256m \
+-Xmx1024m \
+-XX:MaxMetaspaceSize=256m \
+-XX:CompressedClassSpaceSize=128m \
+-Dlogging.config=config/logback.xml \
+-Dspring.config.location=config/application.conf \
+core.jar
