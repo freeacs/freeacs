5c2e1ce4cde32e0e703ffb336a7dba5075bd5718
Jarl André Hübenthal <jarlah@users.noreply.github.com>
2019-03-07 22:22:03 +0100

    make deploy local independant of docker compose file and vice versa

--- scripts/deployLocal.sh
+++ scripts/deployLocal.sh
@@ -2,5 +2,10 @@
 cd "$(dirname "$0")"
 cd ..
 mvn clean package -DskipTests
+declare -a arr=("core" "monitor" "stun" "syslog" "tr069" "web" "webservice")
+for i in "${arr[@]}"
+do
+   docker build "$i" -t freeacs/"$i"
+done
 cd scripts
 docker-compose up -d --build
