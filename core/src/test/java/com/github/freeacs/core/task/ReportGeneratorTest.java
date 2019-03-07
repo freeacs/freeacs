67db3b3f265fba3cffb6a4e044c171baf02aa352
Jarl André Hübenthal <jarlah@users.noreply.github.com>
2019-03-07 21:40:26 +0100

    manage all dependency versions

--- core/src/test/java/com/github/freeacs/core/task/ReportGeneratorTest.java
+++ core/src/test/java/com/github/freeacs/core/task/ReportGeneratorTest.java
@@ -1,6 +1,6 @@
 package com.github.freeacs.core.task;
 
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
