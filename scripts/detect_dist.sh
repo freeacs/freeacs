7b9084521dde603957fdd97479427a1d3a671533
Jarl André Hübenthal <jarlah@users.noreply.github.com>
2019-03-09 19:25:36 +0100

    work in progress

--- scripts/detect_dist.sh
+++ scripts/detect_dist.sh
@@ -1,17 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 # Detects distribution of linux
 
 OS=`uname -s`
 DIST="Unknown"
 
-if [ "${OS}" = "Linux" ] ; then
-	KERNEL=`uname -r`
-	if [ -f /etc/centos-release ] ; then
+if [[ "${OS}" = "Linux" ]] ; then
+	if [[ -f /etc/centos-release ]] ; then
 		DIST='CentOS'
-	elif [ -f /etc/redhat-release ] ; then
-		DIST='RedHat'
-	elif [ -f /etc/debian_version ] ; then
-		DIST="Debian"
+	elif [[ -f /etc/debian_version ]] ; then
+		DIST=`lsb_release -is`
+		if [[ ${DIST} != "Ubuntu" ]]; then
+		    DIST="Unsupported debian distro: $DIST"
+		else
+            CURRENT_VERSION=`lsb_release -rs`
+            REQUIRED_VERSION='16.04'
+            if [[ "$(printf '%s\n' "$REQUIRED_VERSION" "$CURRENT_VERSION" | sort -V | head -n1)" = "$REQUIRED_VERSION" ]];
+            then
+                DIST="Your Ubuntu version is not supported, minimum is ${REQUIRED_VERSION}"
+                exit
+            fi
+        fi
 	fi
 fi
 
