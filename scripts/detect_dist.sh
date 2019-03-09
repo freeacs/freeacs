2843c5d9dd7b958104c8092cef19a69b1692f026
Jarl André Hübenthal <jarlah@users.noreply.github.com>
2019-03-09 19:28:27 +0100

    work in progress

--- scripts/detect_dist.sh
+++ scripts/detect_dist.sh
@@ -9,17 +9,17 @@ if [[ "${OS}" = "Linux" ]] ; then
 		DIST='CentOS'
 	elif [[ -f /etc/debian_version ]] ; then
 		DIST=`lsb_release -is`
-        if [[ ${DIST} != "Ubuntu" ]]; then
-            DIST="Unsupported debian distro: $DIST"
-        else
-            CURRENT_VERSION=`lsb_release -rs`
-            REQUIRED_VERSION='16.04'
-            if [[ "$(printf '%s\n' "$REQUIRED_VERSION" "$CURRENT_VERSION" | sort -V | head -n1)" = "$REQUIRED_VERSION" ]];
-            then
-                DIST="Your Ubuntu version is not supported, minimum is ${REQUIRED_VERSION}"
-                exit
-            fi
-        fi
+		if [[ ${DIST} != "Ubuntu" ]]; then
+			DIST="Unsupported debian distro: $DIST"
+		else
+			CURRENT_VERSION=`lsb_release -rs`
+			REQUIRED_VERSION='16.04'
+			if [[ "$(printf '%s\n' "$REQUIRED_VERSION" "$CURRENT_VERSION" | sort -V | head -n1)" = "$REQUIRED_VERSION" ]];
+			then
+				DIST="Your Ubuntu version is not supported, minimum is ${REQUIRED_VERSION}"
+				exit
+ 			fi
+		fi
 	fi
 fi
 
