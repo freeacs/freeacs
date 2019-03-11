670f95f5e76f97f31633126e8d09d01d94e8b453
Jarl André Hübenthal <jarlah@users.noreply.github.com>
2019-03-11 18:40:43 +0100

    add notice about backup errors

--- scripts/src/main/resources/common/backup_config.sh
+++ scripts/src/main/resources/common/backup_config.sh
@@ -3,6 +3,8 @@
 declare -a moduleNames=(core stun syslog shell monitor web webservice tr069)
 
 for module in ${moduleNames[@]} ; do
+    echo "Dont mind the warnings below, this script is trying to backup two types of property files."
+    echo "Only one type of property file exists in each module"
     config_loc=/opt/freeacs-${module}/config
     config_name=application-config
     # backup .conf file
