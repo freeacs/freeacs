d52d25a65d53737de818a783eaf93d7360aec3e2
Jarl André Hübenthal <jarlah@users.noreply.github.com>
2019-03-18 22:01:07 +0100

    refactoring resolver method a bit

--- rest/src/main/java/com/github/freeacs/rest/graphql/resolvers/UnitResolver.java
+++ rest/src/main/java/com/github/freeacs/rest/graphql/resolvers/UnitResolver.java
@@ -2,6 +2,9 @@ package com.github.freeacs.rest.graphql.resolvers;
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import com.github.freeacs.dbi.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +12,7 @@ import org.springframework.stereotype.Component;
 
 import java.sql.SQLException;
 import java.util.Collection;
-import java.util.Collections;
+import java.util.Optional;
 
 @Component
 public class UnitResolver implements GraphQLQueryResolver {
@@ -22,23 +25,32 @@ public class UnitResolver implements GraphQLQueryResolver {
         this.acsUnit = new ACSUnit(dbi.getAcs().getDataSource(), dbi.getAcs(), dbi.getAcs().getSyslog());
     }
 
+    @Data
+    @AllArgsConstructor
+    private static class Tuple<FIRST, SECOND> {
+        FIRST first;
+        SECOND second;
+    }
+
     @SuppressWarnings("unused")
     public Collection<Unit> getUnits(String search, String unittypeName, String profileName, Integer number) throws SQLException {
-        Unittype unittype = null;
-        Profile profile = null;
-        if (unittypeName != null) {
-            unittype = this.acsUnit.getAcs().getUnittype(unittypeName);
-            if (unittype == null) {
-                log.warn("Unittype " + unittypeName + " does not exist");
-                return Collections.emptyList();
-            } else if (profileName != null) {
-                profile = unittype.getProfiles().getByName(profileName);
-                if (profile == null) {
-                    log.warn("Profile " + profileName + " does not exist in Unittype " + unittypeName);
-                    return Collections.emptyList();
-                }
-            }
-        }
-        return acsUnit.getUnits(search, unittype, profile, number).values();
+        Tuple<Unittype, Profile> tuple = getUnittypeAndProfile(unittypeName, profileName);
+        return acsUnit.getUnits(search, tuple.first, tuple.second, number).values();
+    }
+
+    @NotNull
+    private Tuple<Unittype, Profile> getUnittypeAndProfile(String unittypeName, String profileName) {
+        return Optional.ofNullable(unittypeName)
+                .map(this.acsUnit.getAcs()::getUnittype)
+                .flatMap(unittype -> {
+                    Optional<Tuple<Unittype, Profile>> tuple = Optional.ofNullable(profileName)
+                            .map(unittype.getProfiles()::getByName)
+                            .map(profile -> new Tuple<>(unittype, profile));
+                    if (tuple.isPresent()) {
+                        return tuple;
+                    }
+                    return Optional.of(new Tuple<>(unittype, null));
+                })
+                .orElseGet(() -> new Tuple<>(null, null));
     }
 }
