a70538f641d61e321367202d2e73e62f7b27d249
Jarl André Hübenthal <jarlah@users.noreply.github.com>
2019-04-27 20:03:39 +0200

    adding health endpoint tests

---
+++ tr069/src/test/java/com/github/freeacs/health/NoSecurityHealthTest.java
@@ -0,0 +1,38 @@
+package com.github.freeacs.health;
+
+import com.github.freeacs.Main;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = Main.class)
+@AutoConfigureMockMvc
+@TestPropertySource(locations = {
+        "classpath:application.properties",
+        "classpath:application-h2-datasource.properties",
+        "classpath:application-no-security.properties"
+})
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+public class NoSecurityHealthTest {
+
+    @Autowired
+    protected MockMvc mvc;
+
+    @Test
+    public void canReachHealthEndpoint() throws Exception {
+        mvc.perform(get("/tr069/ok"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("FREEACSOK"));
+    }
+}
