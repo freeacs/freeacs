cd8039f3fde3bea496e18dab932b373bdf393586
Jarl André Hübenthal <jarlah@users.noreply.github.com>
2019-04-27 08:19:59 +0200

    remove unused log methods

--- tr069/src/main/java/com/github/freeacs/tr069/base/Log.java
+++ tr069/src/main/java/com/github/freeacs/tr069/base/Log.java
@@ -2,55 +2,13 @@ package com.github.freeacs.tr069.base;
 
 import com.github.freeacs.dbi.UnitParameter;
 import com.github.freeacs.dbi.util.SystemParameters;
-import java.util.HashMap;
-import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/** This class handles all the logging in this server. */
-@SuppressWarnings("rawtypes")
 public class Log {
   private static Logger eventLog = LoggerFactory.getLogger("Event");
   private static Logger convLog = LoggerFactory.getLogger("Conversation");
   private static Logger debugLog = LoggerFactory.getLogger("Debug");
-
-  private static Map<Class, Logger> loggers = new HashMap<>();
-
-  public static void debug(Class c, String message) {
-    getLogger(c).debug(message);
-  }
-
-  public static void info(Class c, String message) {
-    getLogger(c).info(message);
-  }
-
-  public static void notice(Class c, String message) {
-    getLogger(c).info(message);
-  }
-
-  public static void warn(Class c, String message) {
-    getLogger(c).warn(message);
-  }
-
-  public static void warn(Class c, String message, Throwable t) {
-    getLogger(c).warn(message, t);
-  }
-
-  public static void error(Class c, String message) {
-    getLogger(c).error(message);
-  }
-
-  public static void error(Class c, String message, Throwable t) {
-    getLogger(c).error(message, t);
-  }
-
-  public static void fatal(Class c, String message) {
-    getLogger(c).error(message);
-  }
-
-  public static void fatal(Class c, String message, Throwable t) {
-    getLogger(c).error(message, t);
-  }
 
   public static void conversation(SessionDataI sessionData, String message) {
     convLog.info(message);
@@ -71,14 +29,5 @@ public class Log {
 
   public static void event(String message) {
     eventLog.info(message);
-  }
-
-  private static Logger getLogger(Class loggerId) {
-    Logger logger = loggers.get(loggerId);
-    if (logger == null) {
-      logger = LoggerFactory.getLogger(loggerId);
-      loggers.put(loggerId, logger);
-    }
-    return loggers.get(loggerId);
   }
 }
