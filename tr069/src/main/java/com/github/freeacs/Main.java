2d31b44542ea20a7418775b146ff3ea54d0d761c
Jarl André Hübenthal <jarlah@users.noreply.github.com>
2019-04-29 21:02:43 +0200

    set shutdown hook before initializing spring

--- tr069/src/main/java/com/github/freeacs/Main.java
+++ tr069/src/main/java/com/github/freeacs/Main.java
@@ -10,16 +10,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Main {
 
     public static void main(String[] args) {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            log.warn("Shutdown Hook is running !");
+            Sleep.terminateApplication();
+        }));
         SpringApplication.run(Main.class, args);
-
-        Runtime.getRuntime()
-                .addShutdownHook(
-                        new Thread(
-                                () -> {
-                                    log.warn("Shutdown Hook is running !");
-                                    Sleep.terminateApplication();
-                                }
-                        )
-                );
     }
 }
