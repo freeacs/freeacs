c2cc839f12614c1e23407d285dd5ad4ab909ebbb
dptahtmq1 <dptahtmq1@gmail.com>
2020-07-26 23:55:47 +0900

    [web-v2] initial commit

---
+++ web-v2/src/main/java/com/github/dptahtmq1/webv2/WebV2Application.java
@@ -0,0 +1,13 @@
+package com.github.dptahtmq1.webv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebV2Application.class, args);
+    }
+
+}
---
+++ web-v2/src/test/java/com/github/dptahtmq1/webv2/WebV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.dptahtmq1.webv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
