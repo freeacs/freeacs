4c448c557738df62bdc430fecdc711521a81a702
Jarl André Hübenthal <jarlah@protonmail.com>
2024-01-31 18:19:50 +0100

    add test app

--- tr069-test-app/src/test/java/com/github/test/freeacs/TestMain.java
+++ tr069-test-app/src/test/java/com/github/test/freeacs/TestMain.java
@@ -2,7 +2,6 @@ package com.github.test.freeacs;
 
 import com.github.freeacs.Main;
 import com.github.freeacs.common.util.Sleep;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
@@ -11,14 +10,10 @@ import java.util.Properties;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "com.github.freeacs" })
-@Slf4j
 public class TestMain {
 
     public static void main(String[] args) throws Exception {
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            log.warn("Shutdown Hook is running !");
-            Sleep.terminateApplication();
-        }));
+        Runtime.getRuntime().addShutdownHook(new Thread(Sleep::terminateApplication));
         Properties props = new Properties();
         props.put("spring.test.database.testcontainers", "true");
         new SpringApplicationBuilder(Main.class).properties(props).run(args);
