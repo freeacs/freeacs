49d7fd6b098bd8ad4b381e924ddf91f670084e4a
Jarl André Hübenthal <jarlah@protonmail.com>
2024-01-31 18:22:01 +0100

    refactor

--- tr069-test-app/src/test/java/com/github/test/freeacs/TestMain.java
+++ tr069-test-app/src/test/java/com/github/test/freeacs/TestMain.java
@@ -2,20 +2,16 @@ package com.github.test.freeacs;
 
 import com.github.freeacs.Main;
 import com.github.freeacs.common.util.Sleep;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
-
-import java.util.Properties;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "com.github.freeacs" })
 public class TestMain {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Runtime.getRuntime().addShutdownHook(new Thread(Sleep::terminateApplication));
-        Properties props = new Properties();
-        props.put("spring.test.database.testcontainers", "true");
-        new SpringApplicationBuilder(Main.class).properties(props).run(args);
+        SpringApplication.run(Main.class, args);
     }
 }
