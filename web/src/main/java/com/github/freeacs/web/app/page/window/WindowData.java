d04b9247b3106d82d8cfdf5932e3a5824a15ef5b
Jarl André Hübenthal <jarlah@protonmail.com>
2024-02-03 18:54:31 +0100

    fix

--- web/src/main/java/com/github/freeacs/web/app/page/window/WindowData.java
+++ web/src/main/java/com/github/freeacs/web/app/page/window/WindowData.java
@@ -11,56 +11,16 @@ import java.util.Map;
 @Setter
 @Getter
 public class WindowData extends InputData {
-  /** The download.
-   * -- GETTER --
-   *  Gets the download.
-   *
-   *
-   * -- SETTER --
-   *  Sets the download.
-   *
-   @return the download
-    * @param download the new download
-   */
+  /** The download. */
   private Input download = Input.getStringInput("download");
 
-  /** The regular.
-   * -- GETTER --
-   *  Gets the regular.
-   *
-   *
-   * -- SETTER --
-   *  Sets the regular.
-   *
-   @return the regular
-    * @param regular the new regular
-   */
+  /** The regular. */
   private Input regular = Input.getStringInput("regular");
 
-  /** The frequency.
-   * -- GETTER --
-   *  Gets the frequency.
-   *
-   *
-   * -- SETTER --
-   *  Sets the frequency.
-   *
-   @return the frequency
-    * @param frequency the new frequency
-   */
+  /** The frequency. */
   private Input frequency = Input.getStringInput("frequency");
 
-  /** The page.
-   * -- GETTER --
-   *  Gets the page.
-   *
-   *
-   * -- SETTER --
-   *  Sets the page.
-   *
-   @return the page
-    * @param page the new page
-   */
+  /** The page. */
   private Input page = Input.getStringInput("page");
 
   @Override
