7e60c7f362433bb927ebb4d609522b88628e56a9
Jarl André Hübenthal <jarlah@protonmail.com>
2024-02-06 22:25:24 +0100

    remove unnecessary imports

--- tr069/src/main/java/com/github/freeacs/security/AcsUnitDetailsService.java
+++ tr069/src/main/java/com/github/freeacs/security/AcsUnitDetailsService.java
@@ -8,8 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.sql.SQLException;
--- tr069/src/main/java/com/github/freeacs/security/NoneSpringSecurityConfig.java
+++ tr069/src/main/java/com/github/freeacs/security/NoneSpringSecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 import org.springframework.security.web.savedrequest.NullRequestCache;
 
 @Slf4j
