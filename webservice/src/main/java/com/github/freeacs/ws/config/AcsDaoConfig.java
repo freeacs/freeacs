bb636d3cea79ad2a68f1985537ecaf739badca0d
Jarl André Hübenthal <jarlah@protonmail.com>
2024-02-12 10:24:41 +0100

    chore: fix

--- webservice/src/main/java/com/github/freeacs/ws/config/AcsDaoConfig.java
+++ webservice/src/main/java/com/github/freeacs/ws/config/AcsDaoConfig.java
@@ -1,15 +1,17 @@
 package com.github.freeacs.ws.config;
 
 import com.github.freeacs.dbi.ACSDao;
-import com.github.freeacs.dbi.DBI;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class AcsDaoConfig {
 
     @Bean
-    public ACSDao getACSDao(DBI dbi) {
-        return new ACSDao(dbi.getDataSource());
+    public ACSDao getACSDao(@Qualifier("main") DataSource dataSource) {
+        return new ACSDao(dataSource);
     }
 }
