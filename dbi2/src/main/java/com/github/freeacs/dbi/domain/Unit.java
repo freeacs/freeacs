35e0ee4428c92da5ebdfd21a6dfd904a82741493
Jarl André Hübenthal <jarlah@protonmail.com>
2024-03-06 13:48:31 +0100

    chore: add @With

--- dbi2/src/main/java/com/github/freeacs/dbi/domain/Unit.java
+++ dbi2/src/main/java/com/github/freeacs/dbi/domain/Unit.java
@@ -3,9 +3,11 @@ package com.github.freeacs.dbi.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.With;
 import org.jdbi.v3.core.mapper.Nested;
 
 @Data
+@With
 @NoArgsConstructor
 @AllArgsConstructor
 public class Unit {
--- dbi2/src/main/java/com/github/freeacs/dbi/domain/User.java
+++ dbi2/src/main/java/com/github/freeacs/dbi/domain/User.java
@@ -3,8 +3,10 @@ package com.github.freeacs.dbi.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.With;
 
 @Data
+@With
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
