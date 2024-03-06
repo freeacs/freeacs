08259f53f575532443a11e3a62988babf584fda9
Jarl André Hübenthal <jarlah@protonmail.com>
2024-03-06 11:58:38 +0100

    chore: fix tests

--- dbi2/src/test/java/com/github/freeacs/dbi/repositories/RepositoryTest.java
+++ dbi2/src/test/java/com/github/freeacs/dbi/repositories/RepositoryTest.java
@@ -6,6 +6,8 @@ import org.jdbi.v3.sqlobject.SqlObjectPlugin;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class RepositoryTest implements AbstractMySqlIntegrationTest  {
 
     private static Jdbi jdbi;
@@ -18,22 +20,32 @@ public class RepositoryTest implements AbstractMySqlIntegrationTest  {
 
     @Test
     public void canListUnits() {
-        jdbi.onDemand(UnitRepository.class).listUnits().forEach(System.out::println);
+        var result = jdbi.onDemand(UnitRepository.class).listUnits();
+        assertNotNull(result);
+        assertTrue(result.isEmpty());
     }
 
     @Test
     public void canListUsers() {
-        jdbi.onDemand(UserRepository.class).listUsers().forEach(System.out::println);
+        var result = jdbi.onDemand(UserRepository.class).listUsers();
+        assertNotNull(result);
+        assertEquals(1, result.size());
+        assertEquals("admin", result.get(0).getUsername());
+        assertTrue(result.get(0).getAdmin());
     }
 
     @Test
     public void canListUnitTypes() {
-        jdbi.onDemand(UnitTypeRepository.class).listUnitTypes().forEach(System.out::println);
+        var result = jdbi.onDemand(UnitTypeRepository.class).listUnitTypes();
+        assertNotNull(result);
+        assertTrue(result.isEmpty());
     }
 
     @Test
     public void canListProfiles() {
-        jdbi.onDemand(ProfileRepository.class).listProfiles().forEach(System.out::println);
+        var result = jdbi.onDemand(ProfileRepository.class).listProfiles();
+        assertNotNull(result);
+        assertTrue(result.isEmpty());
     }
 
 }
